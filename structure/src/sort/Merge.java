71d1772cb41b70a96a2a4b46fead5e7a38a9ee66
tsenglying-SH <singletian1996@gmail.com>
2020-08-10 07:38:01 +0800

    mergeSort

---
+++ structure/src/sort/Merge.java
@@ -0,0 +1,51 @@
+package sort;
+
+public class Merge {
+    /* 基本思想:
+    1. 将n个元素从中间切开，分成两部分。（左边可能比右边多1个数）
+    2.将步骤1分成的两部分，再分别进行递归分解。直到所有部分的元素个数都为1。
+    3.从最底层开始逐步合并两个排好序的数列。
+   【时间复杂度】:nlog(n) 且为稳定排序
+   缺点：需要额外的o(n)内存存放临时数组
+
+*/
+    public static void main(String[] args) {
+        int [] a= {10,8,6,4,3,7,8,9,12,19};
+        System.out.println("排序前："+java.util.Arrays.toString(a));
+
+        System.out.println("排序后："+java.util.Arrays.toString(a));
+
+
+
+
+    }
+
+    public static void mergeSort(int[] a,int left, int right) {
+        if(left<right){
+          int mid = (left+right)/2;
+          mergeSort(a,left,mid);//左边归并排序
+          mergeSort(a,mid+1,right);//右边归并排序
+        }
+    }
+
+    public static int[] arraySort(int []a,int [] b,int n, int m){
+        //a,b两个有序数列合并
+        int [] temp = new int[a.length+b.length];
+        int i,j,k;
+        i=j=k=0;
+        while(i<n && j<m){
+            if(a[i]<b[j])
+                temp[k++]=a[i++];
+            else
+                temp[k++]=b[j++];
+        }
+        while(i<n){
+            temp[k++]=a[i++];
+        }
+        while(j<m){
+            temp[k++]=b[j++];
+        }
+        return temp;
+    }
+
+}
